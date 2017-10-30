9079e8b5b070daaad0d4c6be53c41b6d55a7c98c
user <razimograbi44@gmail.com>
2017-10-30 08:18:41 +0200

    Initial commit

---
+++ app/src/main/java/aodai/badran/aodaiproject/LogInActivity.java
@@ -0,0 +1,19 @@
+package aodai.badran.aodaiproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
